199d170d0244449b3fb90bdf77e115e5b321e0a1
isaack0329 <isaaack0329@naver.com>
2017-08-17 14:08:26 +0900

    ssssssssssssssss

--- venv/Lib/middle_bot.py
+++ venv/Lib/middle_bot.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello world!'
+    return 'hello world!3333333333333333333333'
 @app.route('/keyboard')
 def keboard_input():
     return "안녕하세요~~~~~~~"
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
 
